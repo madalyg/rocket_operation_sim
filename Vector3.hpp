76596ce8c71c801d010a29220f8b215550d00e14
Maddi <57687290+mxddi@users.noreply.github.com>
2026-09-06 21:00:56 +0000

    Initialized header file, defined 3D vector class

---
+++ Vector3.hpp
@@ -0,0 +1,20 @@
+#pragma once //Include header only once in file
+
+class Vector3D {
+public:
+    double x, y, z;
+
+    //Constructors
+    Vector3D();
+    Vector3D(double x, double y, double z);
+
+    //Main vector math operations req. for GNC physics
+    Vector3D operator+(const Vector3D& other) const;
+    Vector3D operator-(const Vector3D& other) const;
+    Vector3D operator*(double scalar) const; //Scaling forces
+    
+    double dot(const Vector3D& other) const;
+    Vector3D cross(const Vector3D& other) const;
+    double magnitude() const;
+    Vector3D normalize() const;
+};
