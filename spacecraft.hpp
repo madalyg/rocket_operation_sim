708a3bc9adc0e222271929b74aab4ce39b1f82e5
Maddi <57687290+mxddi@users.noreply.github.com>
2026-09-13 06:54:27 +0000

    Defined setters, thruster command, basic mass/position/velocity/acceleration commands

--- spacecraft.hpp
+++ spacecraft.hpp
@@ -1,3 +1,14 @@
+/// Madaly G 2026
+/**
+ * @file Spacecraft.cpp
+ * @brief Rocket vehicle state properties and variable mass propulsion system.
+ * 
+ * @reference Bate, R. R., Mueller, D. D., & White, J. E. (1971). 
+ *            "Fundamentals of Astrodynamics", Dover Publications. Ch. 1 (Rocket Equation).
+ * @reference Niskanen, S. (2013). "OpenRocket Technical Documentation", 
+ *            Section 3.2: Thrust Vectoring & Throttle Constraints.
+ */
+
 #pragma once
 #include "Vector3D.hpp"
 
@@ -31,6 +42,17 @@ class Spacecraft {
         Vector3D get_acceleration() const { return acceleration; }
         double get_total_mass() const { return dry_mass + fuel_mass; }
         double get_fuel_mass() const { return fuel_mass; }
+
+        //Setters
+        void set_state(const Vector3D& pos, const Vector3D& vel);
+
+        //Flight commands
+        void command_thruster(double throttle_request, const Vector3D& direction_request); //Return the throttle and direction to the propulsion system
+
+        //Physics update commands
+        Vector3D calculate_thrust_force() const;
+        void update_mass(double dt);
+        void update_kinematics(const Vector3D& next_pos, const Vector3D& next_vel, const Vector3D& next_accel);
 };
 
 
